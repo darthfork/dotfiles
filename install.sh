7e1f135ee2bd2253fa5f81f6c504e60495d40da7
Abhishek Rai <rai.abhishek90@gmail.com>
2023-04-24 13:06:56 -0700

    feat: install script for codespaces and alike
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>

--- install.sh
+++ install.sh
@@ -1,37 +1,58 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 # Installation script for Github codespaces and alike
 
-# setup vim
-if vim --version &> /dev/null ; then
-    cp -r .vim/ "$HOME/.vim"
-else
-    printf "==================================\nvim is not available\n==================================\n"
-fi
+# Pull in submoduled vim plugins
+git submodule update --init --recursive
 
-# setup tmux
-if tmux -v &> /dev/null ; then
-    cp -r .tmux.conf "$HOME/.tmux.conf"
-else
-    printf "==================================\ntmux is not available\n==================================\n"
-fi
+# setup vim
+echo "Copying vim config"
+cp -r .vim/ "$HOME/.vim"
 
 # setup zsh
+echo "Installing oh-my-zsh"
 if zsh --version &> /dev/null ; then
-    # change default shell to zsh assuming passwordless sudo
-    chsh -s "$(which zsh)" "$USER"
     # install oh-my-zsh
     sh -c "$(curl -fssl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     # copy my zshrc to HOME
     cp ./.zshrc "$HOME/.zshrc"
     # copy my clone of agnoster theme
     cp .config/zsh-themes/agnoster.zsh-theme "$HOME/.oh-my-zsh/themes/agnoster.zsh-theme"
+    # install zsh syntax completion
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+
 else
-    printf "==================================\nzsh is not available\n==================================\n"
+    printf "==================================\nzsh is not available\ninstall it manually\n==================================\n"
 fi
 
 # copy other configs and scripts
+echo "Installing .local/bin and .config"
 mkdir -p "$HOME/.local/bin" "$HOME/.config"
 cp -r .config/ "$HOME/.config"
 cp -r .local/bin/ "$HOME/.local/bin"
 cp .gitconfig "$HOME/.gitconfig"
+
+# store docker images in /home/
+echo "Configuring docker storage"
+mkdir -p "$HOME/docker"
+echo "{'data-root': '${HOME}/docker'}" > /etc/docker/daemon.json
+
+# Install node16, yarn and coc
+echo "Installing yarn"
+npm install yarn
+echo "Installing COC plugin"
+pushd "$HOME/.vim/pack/plugins/start/coc.nvim/"; yarn install; popd
+
+# setup tmux
+echo "Copying tmux config"
+cp -r .tmux.conf "$HOME/.tmux.conf"
+# ex is exiting with exit code 1 for some reason. This needs to be done last to ensure install.sh works.
+echo "Patching .tmux.conf to set default shell to zsh"
+ex "$HOME/.tmux.conf" << HEREDOC
+6 insert
+set -g default-shell $(which zsh)
+.
+xit
+HEREDOC
