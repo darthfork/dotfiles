b2d437b2d740de55af7fc99726cee304e1b9135e
Abhishek Rai <rai.abhishek90@gmail.com>
2024-11-07 18:03:21 -0800

    chore: Use jq as fixer for json data
    Signed-off-by: Abhishek Rai <rai.abhishek90@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -90,6 +90,7 @@ vim.g.ale_fix_on_save = 1
 vim.g.ale_fixers = {
   ['*'] = { 'remove_trailing_lines', 'trim_whitespace' },
   go = { 'gofmt', 'goimports' },
+  json = { 'jq' },
 }
 
 vim.g.ale_linters = {
